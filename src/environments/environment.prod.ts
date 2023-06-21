ab46e1bbda5b4edb67ae514928b7627ca5a05657
Antonio Fernández Marín <antonio.fernandez@Aytos.local>
2023-06-21 08:34:49 +0200

    cambiando environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   apiUrl: 'https://back-chatpad.onrender.com/chatPad',
   production: true,
   URL : {
-    citas:"https://back-chatpad.onrender.com/chatPad/notas/",
-    ejercicios:"https://back-chatpad.onrender.com/chatPad/chats/",
+    notas:"https://back-chatpad.onrender.com/chatPad/notas/",
+    chats:"https://back-chatpad.onrender.com/chatPad/chats/",
     usuarios:"https://back-chatpad.onrender.com/chatPad/usuarios/"
   },
   location:"https://chat-pad.vercel.app/"
